8ad402be8f6076a3d9235ecf223c0f450bcf53cc
DKyrf97 <danylokyrf@gmail.com>
2022-10-21 20:45:34 -0600

    Delete navigate into Layout Component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
 
@@ -7,7 +7,6 @@ export default function Layout() {
 
     return <Fragment>
         <MainNavigation />
-        <Navigate to={"/quotes"} />
         <main className={classes.main}>
             <Outlet />
         </main>
